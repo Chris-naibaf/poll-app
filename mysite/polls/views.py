fccbcc186ab57994a12f118f42d19d378a497aa6
Christopher Aguilar <chris.naibaf@gmail.com>
2022-09-03 13:45:40 -0500

    Created more views and added parameters to them

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,25 @@
 # from django.shortcuts import render - Esto estaba aquí cuando llegué así
 # que no lo eliminaré por el momento.
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 # Esta es una de las formas más sencillas de crear una vista.
 def index(request):
-    return HttpResponse("Hello, World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
